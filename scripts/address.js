4883627245a436f9e90a725a835e5c395c6d7a81
Matt Reeds <mattlreeds@gmail.com>
2020-01-24 13:50:01 -0600

    funcitoning input fields and save button to display to DOM

---
+++ scripts/address.js
@@ -0,0 +1,14 @@
+const container = document.querySelector("#addressList")
+
+document.querySelector("#saveEntry").addEventListener("click", event => {
+    const personName = document.querySelector("#fullName").value
+    const personAddress = document.querySelector("#address").value
+
+    container.innerHTML += `
+        <section>
+            <h1>${personName}</h1>
+            <div>${personAddress}</div>
+        </section>
+    `
+    console.log(address.value)
+})
